7680027e27db743cb5071b029d8d267e2ddd365d
Julie <jatkin80@ford.com>
2022-04-26 18:21:50 -0600

    add categories

--- app.js
+++ app.js
@@ -6,32 +6,32 @@ let inventory = [{
     name: "+5 Dexterity Vest",
     sell_in: 10,
     quality: 20,
-    category: "",
+    category: "clothing",
 }, {
     name: "Aged Brie",
     sell_in: 2,
     quality: 0,
-    category: "",
+    category: "cheese",
 }, {
     name: "Elixir of the Mongoose",
     sell_in: 5,
     quality: 7,
-    category: "",
+    category: "elixir",
 }, {
     name: "Sulfuras, Hand of Ragnaros",
     sell_in: 0,
     quality: 80,
-    category: "",
+    category: "sulfuras",
 }, {
     name: "Backstage passes to a TAFKAL80ETC concert",
     sell_in: 15,
     quality: 20,
-    category: "",
+    category: "tickets",
 }, {
     name: "Conjured Mana Cake",
     sell_in: 3,
     quality: 6,
-    category: "",
+    category: "conjured",
 }]
 
 
