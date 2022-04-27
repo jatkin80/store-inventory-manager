c3444bb4fd336e8fe5bc7e0c5cd64b406ade3212
Julie <jatkin80@ford.com>
2022-04-26 18:01:53 -0600

    create an array. figure the rest out later

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
 const inventoryManager = document.querySelector('.inventory-manager')
 const reviewInventory = document.querySelector('.review-inventory')
 
+let inventory = [{
+    name: "+5 Dexterity Vest",
+    sell_in: 10,
+    quality: 20,
+    category: "",
+}, {
+    name: "Aged Brie",
+    sell_in: 2,
+    quality: 0,
+    category: "",
+}, {
+    name: "Elixir of the Mongoose",
+    sell_in: 5,
+    quality: 7,
+    category: "",
+}, {
+    name: "Sulfuras, Hand of Ragnaros",
+    sell_in: 0,
+    quality: 80,
+    category: "",
+}, {
+    name: "Backstage passes to a TAFKAL80ETC concert",
+    sell_in: 15,
+    quality: 20,
+    category: "",
+}, {
+    name: "Conjured Mana Cake",
+    sell_in: 3,
+    quality: 6,
+    category: "",
+}]
+
 
 const inventoryManagerForm = document.querySelector('#inventory-manager')
 inventoryManagerForm.addEventListener("submit", (event) => {
     event.preventDefault()
     const formData = new FormData(event.target)
-    const itemEntry = formData.get("item-entry")
-    const sellIn = formData.get("sell-in")
-    const quality = formData.get("quality")
-    const dateAdded = formData.get("date-added-calendar")
+    const item = {
+        name: formData.get("item-entry"),
+        sell_in: formData.get("sell-in"),
+        quality: formData.get("quality")
+    }
+
+
 })
 
 const reviewInventoryForm = document.querySelector('#review-inventory')
@@ -17,4 +52,5 @@ reviewInventoryForm.addEventListener("submit", (event) => {
     event.preventDefault()
     const formData = new FormData(event.target)
     const viewInventory = formData.get("view-inventory-calendar")
+    console.log(viewInventory)
 })
