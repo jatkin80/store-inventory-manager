94327a87986536710d198d953e0f708ac29d55b2
Julie <jatkin80@ford.com>
2022-04-27 16:55:40 -0600

    formulas are working

--- app.js
+++ app.js
@@ -1,63 +1,86 @@
-function Item(name, sell_in, quality) {
-    this.name = name;
-    this.sell_in = sell_in;
-    this.quality = quality;
+const inventoryManager = document.querySelector('.inventory-manager')
+const reviewInventory = document.querySelector('.review-inventory')
+const firstForm = document.querySelector('#first-form')
+const reviewInventoryForm = document.querySelector('#review-inventory')
+
+let inventory = [{
+    item_name: "+5 Dexterity Vest",
+    sell_in: 10,
+    quality: 20,
+    date_added: 'today',
+    category: "",
+}, {
+    item_name: "Aged Brie",
+    sell_in: 2,
+    quality: 0,
+    date_added: 'today',
+    category: "",
+}, {
+    item_name: "Elixir of the Mongoose",
+    sell_in: 5,
+    quality: 7,
+    date_added: 'today',
+    category: "",
+}, {
+    item_name: "Sulfuras, Hand of Ragnaros",
+    sell_in: 0,
+    quality: 80,
+    date_added: 'today',
+    category: "",
+}, {
+    item_name: "Backstage passes to a TAFKAL80ETC concert",
+    sell_in: 15,
+    quality: 20,
+    date_added: 'today',
+    category: "",
+}, {
+    item_name: "Conjured Mana Cake",
+    sell_in: 3,
+    quality: 6,
+    date_added: 'today',
+    category: "",
+}]
+
+firstForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const item = {
+        item_name: formData.get("item-entry"),
+        sell_in: formData.get("sell-in"),
+        quality: formData.get("quality"),
+        date_added: formData.get("date-entered"),
+        category: 'none'
+    }
+    console.log(item)
+    assignCategory(item)
+    checkQuality(item)
+    inventory = [...inventory, item]
+    return inventory
+
+
+})
+
+
+function assignCategory(item) {
+    if (item.item_name.includes("Aged Brie")) {
+        item.category = "Aged Brie"
+    } else if (item.item_name.includes("Sulfuras")) {
+        item.category = "Sulfuras"
+    } else if (item.item_name.includes("Backstage passes")) {
+        item.category = "Backstage passes"
+    } else if (item.item_name.includes("Conjured")) {
+        item.category = "Conjured"
+    }
+    return item
 }
 
-const items = [
-    new Item('+5 Dexterity Vest', 10, 20),
-    new Item('Aged Brie', 2, 0),
-    new Item('Elixir of the Mongoose', 5, 7),
-    new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-    new Item('Conjured Mana Cake', 3, 6),
-];
-update_quality(items);
-
-function update_quality(items) {
-    for (let i = 0; i < items.length; i++) {
-        if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (items[i].quality > 0) {
-                if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                    items[i].quality = items[i].quality - 1
-                }
-            }
-        } else {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1
-                if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (items[i].sell_in < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1
-                        }
-                    }
-                    if (items[i].sell_in < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1
-                        }
-                    }
-                }
-            }
-        }
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            items[i].sell_in = items[i].sell_in - 1;
-        }
-        if (items[i].sell_in < 0) {
-            if (items[i].name != 'Aged Brie') {
-                if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (items[i].quality > 0) {
-                        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                            items[i].quality = items[i].quality - 1
-                        }
-                    }
-                } else {
-                    items[i].quality = items[i].quality - items[i].quality
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-                }
-            }
-        }
+function checkQuality(item) {
+    if (item.category === "Sulfuras") {
+        item.quality = 80
+    } else if (item.quality >= 50) {
+        item.quality = 50
+    } else if (item.quality <= 0) {
+        item.quality = 0
     }
+    return item
 }
